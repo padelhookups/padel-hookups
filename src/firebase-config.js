72e0c6aae08a71ec4c8fc6926038cc4236dbc924
Tiago Pereira <tiago.pereira@peopletopeople.pt>
2025-08-05 17:55:09 +0100

    Initial

---
+++ src/firebase-config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBOW8pVZPA8nlU9xcwjnNTxe7dbqCBxub8",
+  authDomain: "padel-hookups.firebaseapp.com",
+  projectId: "padel-hookups",
+  storageBucket: "padel-hookups.firebasestorage.app",
+  messagingSenderId: "976843288333",
+  appId: "1:976843288333:web:d6e147321c6c37dd3a4a44",
+  measurementId: "G-1BYH7VXPSE"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
