58a1f0c9ae6e09bfc3d7ce42c100d668dcd07b8b
Tiago Pereira <30tiagopereira@gmail.com>
2025-12-14 15:36:23 +0000

    Another Fix

--- src/routes/Community.js
+++ src/routes/Community.js
@@ -513,7 +513,7 @@ const Community = () => {
                     color: "white",
                     textAlign: "start",
                     height: 80,
-                    pb: "env(safe-area-inset-top)"
+                    pt: "env(safe-area-inset-top)"
                 }}>
                 <Box
                     sx={{
@@ -547,7 +547,7 @@ const Community = () => {
                 sx={{
                     px: 0,
                     pt: 0,
-                    height: "Calc(100vh - 140px)",
+                    height: "calc(100vh - 140px - env(safe-area-inset-bottom))",
                     overflow: "auto"
                 }}>
                 {showResults ? (
